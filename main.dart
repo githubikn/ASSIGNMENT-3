d9f7ba4211a5c998139dbcc0f32dabc909010dca
githubikn <101468235+githubikn@users.noreply.github.com>
2022-03-12 20:44:39 +0500

    Add files via upload
    ASSIGNMENT 3

---
+++ main.dart
@@ -0,0 +1,637 @@
+void main() {
+  var half = "   STUDENT NAME : ABDUL MANNAN                     ";
+  print(half);
+  var half2 = "  ______________________________                             ";
+  print(half2);
+  var hakd =
+      "                      %%%%%%QUESTION NO 1%%%%%%%%%               ";
+  print(hakd);
+  var zz =
+      "       ==================================================                                                  ";
+  print(zz);
+  var hi =
+      "            THERE ARE THREE TYPES OF OPERATORS IN DART                      ";
+  print(hi);
+  var zi =
+      "       ===================================================                                                  ";
+  print(zi);
+  var ee = "1) Arithmetic Operator";
+  print(ee);
+  var ei = "  _____________________                     ";
+  print(ei);
+  var fd = "2) Equality and rational operation";
+  print(fd);
+  var ii = "  _________________________________                     ";
+  print(ii);
+  var ca = "3) Logical operator";
+  print(ca);
+  var ji = "  _____________________                     ";
+  print(ji);
+  var ss = "________";
+  print(ss);
+
+  var cc =
+      "                 *********ARITHEMTIC OPERTORS***********                               ";
+  print(cc);
+
+  var aa = "ADD";
+  print(aa);
+  var ssi = "=====";
+  print(ssi);
+
+  num hi1 = 32;
+  num hi2 = 56;
+  num now = hi1 + hi2;
+  print(now);
+  var nn1 = "_________";
+  print(nn1);
+  var wq = "SUBTRACT";
+  print(wq);
+  var ww = "________";
+  print(ww);
+  num num2 = 65;
+  num num3 = 25;
+  num ggi = num2 - num3;
+  print(ggi);
+  var nnnnn = "_________";
+  print(nnnnn);
+  var aaa = "MULTIPLY";
+  print(aaa);
+  var aai = "_________";
+  print(aai);
+  num num4 = 23;
+  num num5 = 6;
+  num ab = num4 * num5;
+  print(ab);
+  var nnnn = "__________";
+  print(nnnn);
+  var ccc = "DIVIDE";
+  print(ccc);
+  var cci = "_______";
+  print(cci);
+  num num6 = 12;
+  num num7 = 5;
+  double iii = num6 / num7;
+
+  print(iii);
+  var nnn = "_____________________________________";
+  print(nnn);
+  var abc = "DIVIDE RETURNING AN INTEGER";
+  print(abc);
+  var aba = "____________________________";
+  print(aba);
+  int nu1 = 12;
+  int nu2 = 5;
+  int ikk = nu1 ~/ nu2;
+
+  print(ikk);
+  var nn = "_____________________________________";
+  print(nn);
+
+  var nul = "GET THE REMAINDER AN INTEGER DIVISION";
+  var nuu = "_____________________________________";
+  print(nul);
+  print(nuu);
+
+  int nu3 = 8;
+  int nu4 = 5;
+  int jjj = nu3 % nu4;
+
+  print(jjj);
+  var abcc =
+      "               *******EQUALITY AND RELATIONAL OPERATORS*******                          ";
+  print(abcc);
+
+  var ss1 = "GREATER THAN";
+  var ssi1 = "____________";
+  print(ss1);
+  print(ssi1);
+
+  num ii1 = 8;
+  num ii2 = 6;
+  bool how = ii1 > ii2;
+  print(how);
+  var kkk = "____________";
+  print(kkk);
+
+  var ss2 = "LESS THAN";
+  var ssi2 = "_________";
+  print(ss2);
+  print(ssi2);
+
+  num ii3 = 32;
+  num ii4 = 76;
+  bool how1 = ii3 < ii4;
+  print(how1);
+  var kkk1 = "____________";
+  print(kkk1);
+
+  var ss3 = "GREATER THAN OR EQUAL TO";
+  var ssi3 = "________________________";
+  print(ss3);
+  print(ssi3);
+
+  num ii5 = 56;
+  num ii6 = 12;
+  bool how2 = ii5 >= ii6;
+  print(how2);
+  var kkk2 = "______________________";
+  print(kkk2);
+
+  var ss4 = "LESS THAN OR EQUAL TO";
+  var ssi4 = "________________________";
+  print(ss4);
+  print(ssi4);
+
+  num ii7 = 45;
+  num ii8 = 23;
+  bool how3 = ii7 <= ii8;
+  print(how3);
+  var kkk3 = "______________________";
+  print(kkk3);
+
+  var ss5 = "EQUALITY";
+  var ssi5 = "_________";
+  print(ss5);
+  print(ssi5);
+
+  num ii9 = 93;
+  num ii10 = 43;
+  bool how4 = ii9 == ii10;
+  print(how4);
+  var kkk4 = "___________";
+  print(kkk4);
+
+  var ss6 = " NOT EQUAL";
+  var ssi7 = "_________";
+  print(ss6);
+  print(ssi7);
+
+  num ii11 = 86;
+  num ii12 = 91;
+  bool how5 = ii11 != ii12;
+  print(how5);
+  var kkk5 = "___________";
+  print(kkk5);
+
+  var abcc1 =
+      "             ******* LOGICAL OPERATORS*******                       ";
+  print(abcc1);
+
+  var ss7 = " AND , &&";
+  var ssi8 = "_________";
+  print(ss7);
+  print(ssi8);
+
+  num ii13 = 86;
+  num ii14 = 91;
+  bool how6 = ii13 < ii14 && ii14 > ii13;
+  print(how6);
+  var kkk6 = "___________";
+  print(kkk6);
+
+  var ss8 = " OR , ||";
+  var ssi9 = "_________";
+  print(ss8);
+  print(ssi9);
+
+  num ii15 = 35;
+  num ii16 = 72;
+  bool how7 = ii15 > ii16 || ii16 < ii15;
+  print(how7);
+  var kkk7 = "___________";
+  print(kkk7);
+
+  var ss9 = " NOT , !";
+  var ssi10 = "_________";
+  print(ss9);
+  print(ssi10);
+
+  num ab1 = 21;
+  num ab2 = 35;
+  bool hhh = !(ab1 > ab2);
+  print(hhh);
+
+  var mars =
+      "                      %%%%%%%%QUESTION NO 2%%%%%%%                     ";
+  print(mars);
+
+  var nos1 = " movie ticket ";
+  print(nos1);
+  var isi1 = "1 movie ticket price is 600";
+  print(isi1);
+  var sms = "5 tickets price is";
+  print(sms);
+
+  num h20 = 600;
+  num h202 = 600;
+  num h203 = 600;
+  num h204 = 600;
+  num h205 = 600;
+  num h123 = h20 + h202 + h203 + h204 + h205;
+  print(h123);
+
+  var jazz =
+      "               %%%%%%%%QUESTION NO 3%%%%%%%%                           ";
+  print(jazz);
+
+  List<int> first = [1, 2, 3, 4, 5, 6, 7];
+  List<int> second = [3, 5, 6, 7, 9, 10];
+  List<int> difference = first.toSet().difference(second.toSet()).toList();
+  print(difference.toString());
+
+  var jazz1 = "               %%%%%%% QUESTION NO 5%%%%%%%                   ";
+  print(jazz1);
+
+  var hp1 = "   %%%%%%THERE ARE FIVE DATA TYPES%%%%%              ";
+  print(hp1);
+
+  var hp80 = "________";
+  print(hp80);
+
+  var hp2 = "NUMBERS";
+  print(hp2);
+  var hp81 = "________";
+  print(hp81);
+
+  var hp82 = "________";
+  print(hp82);
+
+  var hp3 = "STRING";
+  print(hp3);
+
+  var hp83 = "________";
+  print(hp83);
+
+  var hp12 = "BOOLEANS";
+  print(hp12);
+  var hp84 = "________";
+  print(hp84);
+
+  var hp85 = "________";
+  print(hp85);
+
+  var hp20 = "LIST";
+  print(hp20);
+  var hp86 = "________";
+  print(hp86);
+
+  var hp21 = "MAPS";
+  print(hp21);
+  var hp87 = "________";
+  print(hp87);
+
+  var yy1 = "NUMBERS";
+  print(yy1);
+  var hp88 = "________";
+  print(hp88);
+  var yy2 = "Numbers in Dart are used to represent numeric literals";
+  print(yy2);
+
+  var hp89 = "________";
+  print(hp89);
+
+  var yy3 = "STRING";
+  print(yy3);
+
+  var hp810 = "________";
+  print(hp810);
+
+  var yy4 = "Strings represent a sequence of characters";
+  print(yy4);
+
+  var hp811 = "________";
+  print(hp811);
+
+  var yy5 = "BOOLEANS";
+  print(yy5);
+
+  var hp812 = "________";
+  print(hp812);
+
+  var yy6 = "	It represents Boolean values true and false";
+  print(yy6);
+
+  var hp813 = "________";
+  print(hp813);
+
+  var yy7 = "LIST ";
+  print(yy7);
+
+  var hp8143 = "________";
+  print(hp8143);
+  var yy8 = "It is an ordered group of objects";
+  print(yy8);
+
+  var hp815 = "________";
+  print(hp815);
+
+  var yy9 = "MAPS";
+  print(yy9);
+  var hp816 = "________";
+  print(hp816);
+
+  var yy10 = "Its represent a set of values as key-value pairs";
+  print(yy10);
+
+  var nice = "                    %%%%%%%%QUESTION NO 4%%%%%%           ";
+  print(nice);
+
+  var hp817 = "________";
+  print(hp817);
+
+  var nice1 = " AND ,&&";
+  print(nice1);
+
+  var hp818 = "________";
+  print(hp818);
+
+  var nice2 =
+      "And − The operator returns true only if all the expressions specified return true";
+  print(nice2);
+
+  var hp819 =
+      "__________________________________________________________________________________";
+  print(hp819);
+
+  var href = ("2 == 2 ? " "a truth " ":" "a lie ");
+  print(href);
+  var hp820 = "__________________________________________";
+  print(hp820);
+
+  var nice3 = "NULL OPERATOR ,??";
+  print(nice3);
+
+  var nice5 =
+      "this operator returns expression on its left,except if ir is null and if so, it returns right expression:";
+  print(nice5);
+
+  var hp821 =
+      "       _____________________________________________________________________________________________________________";
+  print(hp821);
+
+  var nice6 = "print(0??1); <-o and print(1??null);<-1";
+  print(nice6);
+
+  var hp822 = "       __________________________________________";
+  print(hp822);
+
+  var nice7 =
+      "                       %%%%%%QUESTION NO 13%%%%%%                  ";
+  print(nice7);
+
+  var nice8 = "LIST";
+  print(nice8);
+
+  var nice9 = " FIRST METHOD";
+  print(nice9);
+
+  var hp823 = "________________";
+  print(hp823);
+
+  var myList = [1, 2, 3, 4, 5];
+  print(myList.sublist(1, 3));
+
+  var nice10 = " SECOND METHOD";
+  print(nice10);
+
+  var hp824 = "________________";
+  print(hp824);
+
+  myList.shuffle();
+  print('$myList');
+
+  var nice11 = " THIRD METHOD";
+  print(nice11);
+
+  var hp825 = "________________";
+  print(hp825);
+
+  var descList = [6, 5, 4, 3, 2, 1];
+  print(descList.reversed.toList());
+
+  var nice12 = " FOURTH METHOD";
+  print(nice12);
+
+  var hp826 = "________________";
+  print(hp826);
+
+  var ascList = [1, 2, 3, 4, 5, 6];
+  print(ascList.reversed.toList());
+
+  var nice13 = " FIFTH METHOD";
+  print(nice13);
+
+  var hp827 = "________________";
+  print(hp827);
+
+  var rList = [0, 1, 2, 3, 4, 5, 6];
+  rList.replaceRange(2, 3, [10]);
+  print('$rList');
+
+  var hp829 = "________________";
+  print(hp829);
+
+  var nice70 = "STRING";
+  print(nice70);
+  var hp828 = "________________";
+  print(hp828);
+
+  var nice100 = " FIRST METHOD";
+  print(nice100);
+
+  var hp300 = "________________";
+  print(hp300);
+
+  const string = 'Dart is fun';
+  print(string.substring(0, 4));
+
+  var hp400 = "________________";
+  print(hp400);
+
+  var nice101 = " SECOND METHOD";
+  print(nice101);
+
+  var hp301 = "________________";
+  print(hp301);
+
+  const string1 = 'Dart ' + 'is ' + 'fun!';
+  print(string1);
+
+  var hp30 = "________________";
+  print(hp30);
+
+  var nice102 = " THIRD METHOD";
+  print(nice102);
+
+  var hp302 = "________________";
+  print(hp302);
+
+  const string2 = 'Dart ' 'is ' 'fun!';
+  print(string2);
+
+  var hp50 = "________________";
+  print(hp50);
+
+  var nice103 = " FOURTH   METHOD";
+  print(nice103);
+
+  var hp303 = "________________";
+  print(hp303);
+
+  const string4 = 'dartlang';
+  print('$string4 has ${string.length} letters');
+
+  var hp60 = "________________";
+  print(hp60);
+
+  var nice104 = " FIFTH   METHOD";
+  print(nice104);
+
+  var hp304 = "________________";
+  print(hp304);
+
+  const string22 = 'Dart';
+  final runes = string22.runes.toList();
+  print(runes);
+
+  var hp305 = "________________";
+  print(hp305);
+
+
+
+
+  var noice32 =
+      "                           %%%%%QUESTION NO 9%%%%%%                 ";
+  print(noice32);
+
+  var hp306 = "_____________________________";
+  print(hp306);
+
+  var no1 = " NAMES 5 ILIEGAL VARIABLES NAMES";
+  print(no1);
+
+  var hp307 = "______";
+  print(hp307);
+
+  var hp308 = "No 1";
+  print(hp308);
+
+  var hp309 = "________";
+  print(hp309);
+
+  var hp310 = "UNDERSCORE";
+  print(hp310);
+
+  var hp311 = "______";
+  print(hp311);
+
+  var hp312 = "No 2";
+  print(hp312);
+
+  var hp313 = "________";
+  print(hp313);
+
+  var hp314 = "#";
+  print(hp314);
+
+  var hp315 = "_______";
+  print(hp315);
+
+  var hp316 = "No 3";
+  print(hp316);
+
+  var hp317 = "________";
+  print(hp317);
+
+  var hp318 = "DOLLAR SIGN";
+  print(hp318);
+
+  var hp319 = "___";
+  print(hp319);
+
+  var hp320 = "No 4";
+  print(hp320);
+
+  var hp321 = "________";
+  print(hp321);
+
+  var hp322 =
+      "@ can be used within variables names for example,A,_&@#1 is a valid variable name";
+  print(hp322);
+
+  var hp323 =
+      "__________________________________________________________________________________";
+  print(hp323);
+
+  var hp324 = "___";
+  print(hp324);
+
+  var hp325 = "No 5";
+  print(hp325);
+
+  var hp326 = "________________________________";
+  print(hp326);
+
+  var hp327 = "age_ (ends with an underscore);";
+  print(hp327);
+
+  var hp000 = "________________________________";
+  print(hp000);
+
+  var hp328 = "NAMES OF LEGAL VARIABLES";
+  print(hp328);
+
+  var hp001 = "________________________________";
+  print(hp001);
+
+  var hp329 =
+      "Variable name can consist of letter and alphabets. Keywords are not allowed to use as a variable name. Blank spaces are not allowed in variable name. First character of variable should always be alphabet and cannot be digit.";
+  print(hp329);
+
+  var hp002 =
+      "__________________________________________________________________________________________________________________________________________________________________________________________________________________________________";
+  print(hp002);
+
+  var exe1 = "                                  %%%%%%Question no 6%%%%%%";
+  print(exe1);
+  var hp007 = "___________________";
+  print(hp007);
+
+  var num88 = "table of 7";
+  print(num88);
+  var hp008 = "___________________";
+  print(hp008);
+
+  var num786 = 7;
+  print("${num786}");
+  for (var i = 1; i <= 10; i++) {
+    print("${num786}x ${i}=${num786 * i} ");
+  }
+
+  var num787 = "                      %%%%%%QUESTION NO  8%%%%%%%           ";
+  print(num787);
+
+  List<String> name786 = ["mannan", "faraz", "shahmir"];
+  List<int> score = [497, 356, 407];
+  for (int i = 0; i < 3; i++) {
+    print(
+        "name786 : ${name786[i]}/nscore : ${score[i]}/npercentage : ${(score[i] / 500) * 100}%/n");
+  }
+
+  var hp0099 =
+      "___________________________________________________________________________";
+  print(hp0099);
+
+  var ques = "                    QUESTION NO 10               ";
+  print(ques);
+
+  String place = "Hyderabad";
+  String new_place = place.replaceAll("Hyder", "Islam");
+  print("old name : $place");
+  print("NEW Name : $new_place");
+
+  var hp0098 =
+      "___________________________________________________________________________";
+  print(hp0098);
+}
